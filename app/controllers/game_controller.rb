2556d401a8c89074b1c52d2a48e41d1525636227
Dan <danh@users.noreply.github.com>
2016-10-25 17:33:24 -0500

    final version

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,4 +3,15 @@ class GameController < ApplicationController
 
     render("game/play_rock.html.erb")
   end
+
+  def user_plays_paper
+
+    render("game/play_paper.html.erb")
+  end
+
+  def user_plays_scissors
+
+    render("game/play_scissors.html.erb")
+  end
+
 end
